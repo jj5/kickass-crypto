cff9e045abcf986b9c9bcc1b0d17c9323b9176f0
John Elliot V <jj5@jj5.net>
2023-04-03 07:11:00 +1000

    Work, work...

---
+++ bin/dev/count-kickass.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+##################################################################################################
+#                                                                                                #
+#  ____  __.__        __                           _________                        __           #
+# |    |/ _|__| ____ |  | _______    ______ ______ \_   ___ \_______ ___.__._______/  |_  ____   #
+# |      < |  |/ ___\|  |/ /\__  \  /  ___//  ___/ /    \  \/\_  __ <   |  |\____ \   __\/  _ \  #
+# |    |  \|  \  \___|    <  / __ \_\___ \ \___ \  \     \____|  | \/\___  ||  |_> >  | (  <_> ) #
+# |____|__ \__|\___  >__|_ \(____  /____  >____  >  \______  /|__|   / ____||   __/|__|  \____/  #
+#         \/       \/     \/     \/     \/     \/          \/        \/     |__|                 #
+#                                                                                                #
+#                                                                                        By jj5  #
+#                                                                                                #
+##################################################################################################
+
+##################################################################################################
+#
+# 2023-04-03 jj5 - this script counts how many times "kickass" appears in the code.
+#
+##################################################################################################
+
+main() {
+
+  set -euo pipefail;
+
+  pushd "$( dirname "$0" )/../../" >/dev/null;
+
+  local kickass_1=$( egrep -Ri '.*kickass' 2>/dev/null | wc -l );
+  local kickass_2=$( egrep -Ri '.*kickass.*kickass' 2>/dev/null | wc -l );
+  local kickass_3=$( egrep -Ri '.*kickass.*kickass.*kickass' 2>/dev/null | wc -l );
+  local kickass_4=$( egrep -Ri '.*kickass.*kickass.*kickass.*kickass' 2>/dev/null | wc -l );
+
+  # 2023-04-03 jj5 - the +1 is for the last line above (it has five kickasses in it, I'm gonna
+  # assume there are no other instances of five kickasses on a single line).
+
+  local count=$(( kickass_1 + kickass_2 + kickass_3 + kickass_4 + 1 ));
+
+  if false; then
+
+    echo $kickass_1;
+    echo $kickass_2;
+    echo $kickass_3;
+    echo $kickass_4;
+
+  fi
+
+  echo $count;
+
+}
+
+main "$@";
