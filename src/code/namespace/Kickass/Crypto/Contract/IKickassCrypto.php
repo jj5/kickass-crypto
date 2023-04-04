cc536c0eac9877beab6542204cb9ba72eac54a92
John Elliot V <jj5@jj5.net>
2023-04-04 20:16:10 +1000

    Work, work...

--- src/code/namespace/Kickass/Crypto/Contract/IKickassCrypto.php
+++ src/code/namespace/Kickass/Crypto/Contract/IKickassCrypto.php
@@ -18,8 +18,7 @@
 // 2023-04-03 jj5 - a crypto component will provide this interface...
 //
 // 2023-04-03 jj5 - oh man, I really wanted to use the PHP 8.0 type system, but the demo server
-// for this library is still on 7.4. No typed interface for you. :(
-//
+// for this library is still on 7.4 so no typed interface. :(
 //
 \************************************************************************************************/
 
